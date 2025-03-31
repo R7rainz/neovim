cc24510bce16f8a313d879d97ce5a1eaca3425ac
Ronak Kamboj <ronakkamboj26@gmail.com>
2025-03-31 16:41:04 +0530

    New clean start

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    name = "treesitter",
+    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+    build = ":TSUpdate",
+    event = {
+      "BufReadPost",
+      "BufNewFile",
+    },
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "vimdoc",
+          "lua",
+          "toml",
+          "json",
+          "html",
+          "css",
+          "javascript",
+          "typescript",
+          "tsx",
+          "markdown",
+          "python",
+          "cpp",
+          "java",
+          "dockerfile",
+          "bash",
+        },
+        ignore_install = {},
+        sync_install = false,
+        auto_install = false,
+        modules = {},
+        highlight = {
+          enable = true,
+          use_languagetree = true,
+        },
+        indent = { enable = true },
+      })
+    end,
+
+    vim.filetype.add({
+      pattern = {
+        [".env.*"] = "sh",
+      },
+    }),
+  },
+}

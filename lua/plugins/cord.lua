273a23d39001ad0b0c56e389d5fa1258df00f607
Ronak Kamboj <ronakkamboj26@gmail.com>
2025-04-01 12:15:21 +0530

    added cord.lua

---
+++ lua/plugins/cord.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "vyfor/cord.nvim",
+    branch = "master",
+    build = ":Cord update",
+    event = "VeryLazy",
+    opts = {
+      advanced = {
+        plugin = {
+          -- log_level = vim.log.levels.OFF,
+        },
+      },
+    },
+  },
+}
